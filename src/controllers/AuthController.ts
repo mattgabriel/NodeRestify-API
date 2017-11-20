db05279681c8f6e05b72c0c52940550492b90646
Matt Gabriel <matt.g.martire@gmail.com>
2017-11-20 11:46:17 +0100

    route samples

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,7 +2,7 @@ import * as restify from "restify";
 
 export default class AuthController {
 	public get(req: restify.Request, res: restify.Response, next: restify.Next) {
-		res.json(200, {"message": "test 1234"});
+		res.json(200, {"message": "test 1234 " + req.params.user});
 		return next();
 	}
 }
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,9 +2,10 @@ import * as errors from "restify-errors";
 import * as restify from "restify";
 import authController from "../controllers/AuthController";
 
+// curl -X GET http://localhost:3000/auth/someUser -H 'accept-version: ~3'
 function authRoute(server: restify.Server) {
 	const routeCtrl = new authController();
-	server.get("/auth", routeCtrl.get);
+	server.get({path: "/auth/:user", version: "3.0.0"}, routeCtrl.get);
 
 }
 
